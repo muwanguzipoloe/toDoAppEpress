0951b6c48736a703ef293c9a7c19894ee9ab7a25
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-08-11 09:05:53 +0300

    updated app functionality with deletion of tasks

--- app.js
+++ app.js
@@ -3,7 +3,41 @@ loadEvents();
 // load every event in the page
 function loadEvents(){
     document.querySelector('form').addEventListener('submit', submit);
+    document.getElementById('clear').addEventListener('click',clearList);
+    // recently added
+    document.querySelector('ul').addEventListener('click',clearList);
 }
+// delete Tick
+function deleteOrTick(e){
+    if(e.target.className == 'delete')
+        deleteTask(e);
+    else {
+        tickTask(e);
+    }
+}
+// delete task
+function deleteTask(e){
+    let remove = e.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode = removeChild(remove);
+}
+// tick a task
+function tickTask(e){
+    const task = e.target.nextSibling;
+    if(e.target.checked){
+        task.style.textDecoration = "line-through";
+        task.style.color = "#ff0000";
+    }else{
+        task.style.textDecoration = "none";
+        task.style.color = "#2f4f4f";
+    }
+}
+
+function clearList(e){
+    // setting the ul innerHTML to an empty string
+    let ul = document.querySelector('ul').innerHTML = '';
+}
+
 
 // submit data function
 function submit(e){
